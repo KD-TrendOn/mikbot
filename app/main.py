17d1f9500762940f370a3f49a1d3df4efb89d59b
Karfidov <karfidov.work@gmail.com>
2024-08-25 09:12:50 +0500

    replacing FastAPI.on_event("startup")

--- app/main.py
+++ app/main.py
@@ -9,66 +9,75 @@ from langchain_core.messages import ToolMessage
 from .database.crud import create_chat_message
 from .services.router import update_router_parser
 from app.database.connection import AsyncSessionLocal
-app = FastAPI()
+from contextlib import asynccontextmanager
+
 main_graph = None
+
 
 logging.basicConfig(level=logging.ERROR)
 logger = logging.getLogger(__name__)
 
-@app.on_event("startup")
-async def startup_event():
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     global main_graph
     async with AsyncSessionLocal() as db:
         await update_router_parser(db)
         main_graph = await create_main_graph(db)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 @app.post("/chat")
 async def chat(user_input: UserInput, db: AsyncSession = Depends(get_db)):
     try:
         # Сохраняем сообщение пользователя
-        await create_chat_message(db, user_input.user_id, "user", {"message": user_input.user_input})
+        await create_chat_message(
+            db, user_input.user_id, "user", {"message": user_input.user_input}
+        )
 
         initial_state = State(
             user_input=user_input.user_input,
             user_id=user_input.user_id,
-            metadata={"db": db}
+            metadata={"db": db},
         )
-        
+
         result = await main_graph.ainvoke(initial_state)
-        
+
         # Обработка результата
-        pre_answer = result['answer'][0]
+        pre_answer = result["answer"][0]
         print(result)
         if isinstance(pre_answer, ToolMessage):
-            answer:dict = json.loads(pre_answer.content)
+            answer: dict = json.loads(pre_answer.content)
             # Сохраняем ответ бота
             print(answer)
-            resultt = answer.get('result', {})
+            resultt = answer.get("result", {})
             tipe = resultt.pop("type")
-            description = answer.get('description', '')
-            await create_chat_message(db, user_input.user_id, "bot", {
-                "service": result.get('service'),
-                "type": tipe,
-                "tool_call": resultt,
-                "message": description
-            })
-            return {
-                "tool_call": resultt,
-                "type": tipe,
-                "message": description
-            }
+            description = answer.get("description", "")
+            await create_chat_message(
+                db,
+                user_input.user_id,
+                "bot",
+                {
+                    "service": result.get("service"),
+                    "type": tipe,
+                    "tool_call": resultt,
+                    "message": description,
+                },
+            )
+            return {"tool_call": resultt, "type": tipe, "message": description}
         else:
             answer = pre_answer.content
             print("!!!", answer)
-            await create_chat_message(db, user_input.user_id, "bot", {
-                "service": result.get('service'),
-                "message": answer
-            })
-            return {
-                "tool_call": {},
-                "type":'text',
-                "message": answer
-            }
+            await create_chat_message(
+                db,
+                user_input.user_id,
+                "bot",
+                {"service": result.get("service"), "message": answer},
+            )
+            return {"tool_call": {}, "type": "text", "message": answer}
     except Exception as e:
         logger.exception("An error occurred during chat processing")
         raise HTTPException(status_code=500, detail=str(e))
--- run.py
+++ run.py
@@ -1,19 +1,25 @@
 import asyncio
-from app.main import app, startup_event
+from app.main import app
 from app.database.connection import AsyncSessionLocal
 from app.services.service_creator import init_real_services
 import uvicorn
 
+
 async def init_data():
     async with AsyncSessionLocal() as db:
         await init_real_services(db)
-    await startup_event()
+
 
 async def main():
-    await init_data()
+    # await init_data()
     config = uvicorn.Config(app, host="0.0.0.0", port=8000, loop="asyncio")
     server = uvicorn.Server(config)
     await server.serve()
 
+
 if __name__ == "__main__":
+    import sys
+
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.run(main())
